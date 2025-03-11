4f19b924326b0e237d135fd0f2bc4a058951fac6
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-03-11 11:15:10 +0300

    update use debounce logic

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,34 @@
+import { queryClient } from "../api/apiClient";
+import { useEffect, useRef } from "react";
+import { UseMutationResult } from "react-query";
+
+export const useDebouncedMutation = (
+  mutation: UseMutationResult<any, unknown, object, unknown>,
+  query: string,
+  delay = 1500
+) => {
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const debouncedMutate = (variables) => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(() => {
+      mutation.mutate(variables, {
+        onSuccess: () => {
+          queryClient.invalidateQueries(query);
+        },
+      });
+    }, delay);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, []);
+
+  return debouncedMutate;
+};
